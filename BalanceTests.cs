f8bdce72d20f6e157c716f59167d3b238ef0aab5
rutviee002 <rutviee002@gmail.com>
2017-12-18 00:57:18 -0800

    test completed

---
+++ BalanceTests.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using demo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo.Tests
+{
+    [TestClass()]
+    public class BalanceTests
+    {
+        [TestMethod()]
+        public void TestExceptionCase()
+        {
+            Balance bank = new Balance("rutvi",0);
+            try
+            {
+                bank.Debit(2);
+
+            }
+            catch(ArgumentOutOfRangeException e)
+            {
+                StringAssert.Contains(e.Message, "amount<=0 r amount>balance");
+                return;
+         
+
+            }
+            catch(Exception e)
+            {
+                StringAssert.Contains(e.Message, "balance=0");
+                return;
+
+
+            }
+            Assert.Fail("no exception");
+        }
+        [TestMethod()]
+        public void TestCreditCase()
+        {
+            Balance bank = new Balance("rutvi", 2);
+            bank.Credit(2);
+            Assert.AreEqual(4, bank.bal);
+        }
+        [TestMethod()]
+        public void TestDebitCase()
+        {
+            Balance bank = new Balance("rutvi", 2);
+            bank.Debit(2);
+            Assert.AreEqual(0, bank.bal);
+        }
+
+        [TestMethod()]
+        public void TestCreditDebitCase()
+        {
+            Balance bank = new Balance("rutvi", 2);
+            bank.Debit(2);
+            bank.Credit(2);
+            Assert.AreEqual(2, bank.bal);
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ balance.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo
+{
+   public class Balance
+    {
+        string customername;
+        double _balance;
+
+        public Balance( string customername1, double bal)
+        {
+            customername = customername1;
+            _balance = bal;
+
+        }
+public double bal { get { return _balance; } }
+
+        public void Debit(double amount)
+        {
+            if (_balance == 0)
+                throw new Exception("balance=0");
+            if (amount <= 0 || amount > _balance)
+                throw new ArgumentOutOfRangeException("amount<=0 r amount>balance");
+            _balance -= amount;
+
+        }
+        public void Credit(double amount)
+        {
+            if (amount <= 0)
+                throw new ArgumentOutOfRangeException("amount<=0");
+            _balance += amount;
+
+        }
+    }
+}
